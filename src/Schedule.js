9c905f4853fbaecc2b9fe71ce4a64b056502ed6f
Adrien Rousselet <adrien@cogitstudio.com>
2017-07-10 10:31:35 +0200

    Remove Schdule's useless formatting

--- src/Schedule.js
+++ src/Schedule.js
@@ -2,17 +2,10 @@ import React, { Component } from 'react';
 import './Schedule.css';
 
 export default class Schedule extends Component {
-	formatSchedule(schedule) {
-		if(!isNaN(parseInt(schedule, 10))){
-			schedule += " min";
-		}
-		return schedule;
-	}
-
 	render() {
 		return (
 			<div className="schedule">
-				{this.formatSchedule(this.props.schedule)}
+				{this.props.schedule}
 			</div>
 		)
 	}
